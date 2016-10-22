df3530b0dfbf63b2c2340712b23ff4f98280fed7
jprittie <eyewallop@gmail.com>
2016-10-21 21:29:16 -0400

    Changes directory structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,109 +1,116 @@
 'use strict';
 
 // Load node modules/plugins
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var eslint = require('gulp-eslint');
-var uglify = require('gulp-uglify');
-var uglifycss = require('gulp-uglifycss');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var imagemin = require('gulp-imagemin');
-var maps = require('gulp-sourcemaps');
-var del = require('del');
-var browserSync = require('browser-sync').create();
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  eslint = require('gulp-eslint'),
+  uglify = require('gulp-uglify'),
+  uglifycss = require('gulp-uglifycss'),
+  rename = require('gulp-rename'),
+  sass = require('gulp-sass'),
+  imagemin = require('gulp-imagemin'),
+  maps = require('gulp-sourcemaps'),
+  del = require('del'),
+  browserSync = require('browser-sync').create();
 
-
-// Instead of putting all the JS-related tasks into one 'scripts' task, I am breaking them down into smaller tasks. Is that the right thing to do?
-
-
-// Concatenate scripts task
-// This task generates source maps for the JS files
-// It then concatenates the JS files and pipes them to the js/all folder
-// ** Was it correct to pipe them to a js/all folder?
-gulp.task('concatScripts', function(){
-  return gulp.src(['js/*.js', 'js/**/*.js'])
-    .pipe(maps.init())
-    .pipe(concat('all.js'))
-    .pipe(maps.write('./'))
-    .pipe(gulp.dest('js/all'));
-});
-
-// Minify scripts task
-// This concatenates the scripts before minifying them
-// It will then rename the result as all.min.js and place it in dist/scripts
-// ** It also uses browser-sync to update browser with new JS, but is this the correct place to do that?
-gulp.task('minifyScripts', ['concatScripts'], function(){
-  return gulp.src('js/all/all.js')
-    .pipe(uglify())
-    .pipe(rename('all.min.js'))
-    .pipe(gulp.dest('dist/scripts'))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
-});
 
 // Lint task
-// This task will lint the scripts, and be used to halt the build process if there's an error
 gulp.task('lintScripts', function(){
- return gulp.src(['js/*.js', 'js/*/*.js'])
+ return gulp.src(['app/js/global.js', 'app/js/circle/*.js'])
+  // This will lint the scripts
    .pipe(eslint())
+   // Then output any warnings/errors to console
    .pipe(eslint.format())
+   // And halt the build process if there's an error
    .pipe(eslint.failAfterError());
 });
 
-// Scripts task
-// This task will lint, concatenate and minify all JS files
-// It will also rename the result and place it in dist/scripts
-// I have set up the task this way because I want to make sure the scripts are linted before they are minified
-gulp.task('scripts', ['lintScripts'], function(){
-  return gulp.start('minifyScripts');
-});
-
-// Styles task
-// First, this generates source maps for the SCSS files
-// Then it compiles SCSS files into CSS
-// It concatenates and minifies the CSS into all.min.css
-// The result is copied to the dist/styles folder
-// browser-sync is used to inject new CSS styles into the browser whenever the styles task is run
-gulp.task('styles', function(){
-  return gulp.src(['sass/*.scss', 'sass/**/*.sass', 'sass/**/**/*.sass'])
+// Concatenate scripts task
+gulp.task('concatScripts', ['lintScripts'], function(){
+  return gulp.src(['app/js/global.js', 'app/js/circle/*.js'])
+    // Generate source maps for JS files
     .pipe(maps.init())
-    .pipe(sass())
-    .pipe(concat('all.min.css'))
-    .pipe(uglifycss())
+    // Concatenate JS files into file named all.js
+    .pipe(concat('all.js'))
     .pipe(maps.write('./'))
-    .pipe(gulp.dest('dist/styles'))
+    // Pipe result to the js/all folder
+    .pipe(gulp.dest('app/js/all'))
+    // Reload browser with updated JS
     .pipe(browserSync.reload({
       stream: true
     }));
 });
 
+// Scripts task
+gulp.task('scripts', ['concatScripts'], function(){
+  return gulp.src('app/js/all/all.js')
+    // Take all.js file created in concatScripts and minify it
+    .pipe(uglify())
+    // Then rename resulting file all.min.js
+    .pipe(rename('all.min.js'))
+    // Copy it to js/all
+    .pipe(gulp.dest('app/js/all'))
+    // And also copy it to dist/scripts
+    .pipe(gulp.dest('dist/scripts'))
+});
+
+// Compile sass task
+gulp.task('compileSass', function(){
+  return gulp.src(['app/sass/global.scss'])
+    // Generate source maps for the SCSS files
+    .pipe(maps.init())
+    // Compile those files into CSS
+    .pipe(sass())
+    // Name the resulting file all.css
+    .pipe(rename('all.css'))
+    // Write source maps
+    .pipe(maps.write('./'))
+    // Save the result to css folder
+    .pipe(gulp.dest('app/css'))
+    // Use browser-sync to inject new CSS styles into the browser
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+// Styles task
+gulp.task('styles', ['compileSass'], function(){
+  return gulp.src(['app/css/all.css'])
+    // Minify the CSS
+    .pipe(uglifycss())
+    // Rename as all.min.css
+    .pipe(rename('all.min.css'))
+    // Copy result to css folder
+    .pipe(gulp.dest('app/css'))
+    // The result is copied to the dist/styles folder
+    .pipe(gulp.dest('dist/styles'))
+});
+
 // Optimize images task
-// This optimizes JPEG and PNG images, and copies them to dist/content
 gulp.task('images', function(){
-  return gulp.src('images/*')
+  return gulp.src('app/images/*')
+    // Optimize JPEG and PNG images
     .pipe(imagemin())
+    // Copies them to dist/content
     .pipe(gulp.dest('dist/content'));
 });
 
 // Clean task
 // Deletes all files and folders in the dist folder
-// It also deletes the js/all folder, which I created as an intermediate step in concatScripts
+// It also deletes the js/all and css folders
 gulp.task('clean', function(){
-  return del(['dist', 'js/all']);
+  return del(['dist', 'app/js/all', 'app/css']);
 });
 
 // Build task
 // Runs clean, scripts, styles and images tasks
 // But clean must complete first, so it is a dependency and the other tasks run in the callback
 gulp.task('build', ['clean'], function(){
-  gulp.start('scripts', 'styles', 'images');
+  gulp.start(['scripts', 'styles', 'images']);
   // This task also moves index.html and icon files to the dist folder
-  gulp.src(['index.html', 'icons/**'], {base: './'})
+  return gulp.src(['app/index.html', 'app/icons/**'], {base: './app'})
     .pipe(gulp.dest('dist'));
 });
-
 
 // Default task
 // This makes 'build' the default task so it runs when 'gulp' is run from the command line
@@ -112,21 +119,21 @@ gulp.task('default', function(){
 });
 
 // Serve task
-// gulp serve from command line should build and serve the project using a local web server
+// Running 'gulp serve' from command line will build and serve project using a local web server
 gulp.task('serve', ['build'], function(){
   browserSync.init({
     server: {
-      baseDir: 'dist'
+      baseDir: 'app'
     }
   })
-  gulp.start('watch');
+  // It will also start the watch task, which in turn will reload browser when changes are made to JS and SCSS files
+  return gulp.start('watch');
 });
 
 // Watch task
-// ** In the video, Huston only used concatScripts in the watch task
-// ** I believe the rationale was that minifying takes too much time, so you wouldn't want to do it anytime you save a file.
-// ** So should I have just used concatScripts here?
 gulp.task('watch', function(){
-  gulp.watch(['js/global.js', 'js/circle/*.js'], ['scripts']);
-  gulp.watch(['sass/*.scss', 'sass/**/*.sass', 'sass/**/**/*.sass'], ['styles']);
+  // Run concatScripts when JS files are updated
+  gulp.watch(['app/js/global.js', 'app/js/circle/*.js'], ['concatScripts']);
+  // Run styles task when SCSS or SASS files are updated
+  gulp.watch(['app/sass/*.scss', 'app/sass/**/*.sass', 'app/sass/**/**/*.sass'], ['styles']);
 });
